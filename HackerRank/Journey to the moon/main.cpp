2e402f23601ab0980dbbd72893685a47daaffbb9
Friedrich Doku <friedrichdoku@gmail.com>
2018-04-12 06:26:43 -0500

    Minor Changes

--- HackerRank/Journey to the moon/main.cpp
+++ HackerRank/Journey to the moon/main.cpp
@@ -5,39 +5,47 @@ using namespace std;
 
 #define MAX 1000000
 
+int verts = 0;
 vector<int> *graph;
+vector<bool> visited;
 
-void DFSUtil(int v, bool visited[]){
-    // Mark the current node as visited
+// Find all reachable nodes
+void DFS(int v){
     visited[v] = true;
 
-    // Recur for all adjecent nodes
-    vector<int>::iterator it;
-    for(it = graph[v].begin(); it != graph[v].end(); ++it){
+    verts++;
+
+    for(vector<int>::iterator it = graph[v].begin(); it != graph[v].end(); ++it){
         if(!visited[*it]){
             visited[*it] = true;
+            DFS(*it);
         }
     }
 }
 
-// Find all reachable nodes
-void DFS(int v, int number_of_ver){
-    bool *visited = new bool[number_of_ver];
-    for(int i = 0; i < number_of_ver; i++){
-        visited[i] = false;
+int factorial(int n){
+    if(n == 1){
+        return n;
     }
-
-    DFSUtil(v, visited);
+    return n*factorial(n-1);
 }
 
 int main(){
     int N, P;
     cin >> N >> P;
+    vector<int> repre;
 
     graph = new vector<int>[N];
     while(P--){
         int v, e;
         graph[v].push_back(e);
+        graph[e].push_back(v);
+    }
+
+    for(int i = 0; i < N; i++){
+        DFS(i); 
+        repre.push_back(verts); 
+        verts = 0; 
     }
     return 0;
 }
