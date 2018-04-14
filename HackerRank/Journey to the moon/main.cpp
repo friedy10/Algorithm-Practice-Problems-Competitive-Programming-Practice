b88972c1742888624fb6a0a6081de0b54b82cb8b
Friedrich Doku <friedrichdoku@gmail.com>
2018-04-13 20:50:39 -0500

    Journey To The Moon

--- HackerRank/Journey to the moon/main.cpp
+++ HackerRank/Journey to the moon/main.cpp
@@ -3,41 +3,35 @@
 
 using namespace std;
 
-#define MAX 1000000
+#define MAX 10000000
 
 int verts = 0;
 vector<int> *graph;
-vector<bool> visited;
+vector<int> visited(MAX);
 
 // Find all reachable nodes
 void DFS(int v){
-    visited[v] = true;
+    visited[v] = 1;
 
     verts++;
 
     for(vector<int>::iterator it = graph[v].begin(); it != graph[v].end(); ++it){
         if(!visited[*it]){
-            visited[*it] = true;
+            visited[*it] = 1;
             DFS(*it);
         }
     }
 }
 
-int factorial(int n){
-    if(n == 1){
-        return n;
-    }
-    return n*factorial(n-1);
-}
-
 int main(){
-    int N, P;
+    int N, P, numC;
     cin >> N >> P;
     vector<int> repre;
 
     graph = new vector<int>[N];
     while(P--){
         int v, e;
+        cin >> v >> e;
         graph[v].push_back(e);
         graph[e].push_back(v);
     }
@@ -45,7 +39,17 @@ int main(){
     for(int i = 0; i < N; i++){
         DFS(i); 
         repre.push_back(verts); 
+        numC++;
         verts = 0; 
     }
+
+    int totalWays = N * (N - 1) / 2;
+    int sameWays = 0;
+    
+    for(int i = 0; i < numC; i++)
+    {    
+        sameWays = sameWays + (repre[i]*(repre[i]-1) / 2);
+    }
+    cout << (totalWays - sameWays) << endl;
     return 0;
 }
